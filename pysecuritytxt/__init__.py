4b2e1f8f60c5e871c2357c8cc07a3deefc2b15a9
Raphaël Vinot <raphael@vinot.info>
2024-01-13 19:30:51 +0100

    chg: Run mypy in strict mode

--- pysecuritytxt/__init__.py
+++ pysecuritytxt/__init__.py
@@ -3,7 +3,10 @@ import argparse
 from .api import PySecurityTXT, SecurityTXTNotAvailable  # noqa
 
 
-def main():
+__all__ = ['PySecurityTXT', 'SecurityTXTNotAvailable']
+
+
+def main() -> None:
     parser = argparse.ArgumentParser(description='Try to get a security.txt file')
     parser.add_argument('url_or_domain', help='Try to get the file from there.')
     parser.add_argument('-p', '--parse', default=False, action='store_true', help='Parse the response, returns dict')
--- tests/test.py
+++ tests/test.py
@@ -8,13 +8,13 @@ from pysecuritytxt import PySecurityTXT
 
 class TestBasic(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = PySecurityTXT()
 
-    def test_basic(self):
+    def test_basic(self) -> None:
         txt = self.client.get('circl.lu')
         self.assertTrue(txt)
 
-    def test_securitytxt_in_root(self):
+    def test_securitytxt_in_root(self) -> None:
         txt = self.client.get('liu.se')
         self.assertTrue(txt)
